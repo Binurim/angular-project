46ec9567dae650625a6d5a4ef32e30cfe6f3af81
binuri <binurim95@gmail.com>
2024-12-26 02:06:09 +0530

    Refreshing basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SuccessAlertComponent } from "./success-alert/success-alert.component";
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SuccessAlertComponent, WarningAlertComponent, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  username: String = 'testUsername';
   title = 'angular-project';
+  displayPara: boolean = false;
+  buttonClickLogs: number[] = [];
+
+  onClickUsername() {
+    this.username = '';
+  }
+
+  onDisplayDetails() {
+    this.displayPara = !this.displayPara;
+    this.buttonClickLogs.push(this.buttonClickLogs.length + 1)
+  }
 }
